feaf4543c785d0d86865fcb4238d2dd6a4f284f8
Mohammed <stankurd357@gmail.com>
2026-08-13 12:34:39 +0300

    Add service worker for offline caching
    Implement service worker for caching files.

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'subedit-cache-v1';
+
+// لیستی ئەو فایلانەی کە دەبێت خەزن ببن بۆ کاتی بێ ئینتەرنێتی
+const urlsToCache = [
+    './',
+    './index.html'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
