d8df157ee166519e704e15a9127737f47707196d
Mohammed <stankurd357@gmail.com>
2026-08-19 18:46:46 +0300

    Update cache name to subedit-v2.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'subedit-v2.3';
+const CACHE_NAME = 'subedit-v2.4';
 
 // دۆزینەوەی خۆکاری ناوی بوخچەی GitHub Pages
 const GH_PATH = self.location.pathname.replace(/\/sw\.js$/, '');
