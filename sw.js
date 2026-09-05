7e1fdbacd9b4fd1c549a1a91166c69439009b921
Mohammed <stankurd357@gmail.com>
2026-09-05 11:34:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'subedit-v4.0025';
+const CACHE_NAME = 'subedit-v4.0026';
 
 // دۆزینەوەی خۆکاری ناوی بوخچەی GitHub Pages
 const GH_PATH = self.location.pathname.replace(/\/sw\.js$/, '');
